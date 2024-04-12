1ed4c0f507c1995d6375dd197c19c53d07f44b81
haitrienotd <166778538+haitrienotd@users.noreply.github.com>
2024-04-12 10:10:48 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("ALhqRUcwYAFTu3L4zMU8HZiqWekeL6cQcfnkmYN7CpkW");
 
 #[program]
 pub mod nft_lend_borrow {
